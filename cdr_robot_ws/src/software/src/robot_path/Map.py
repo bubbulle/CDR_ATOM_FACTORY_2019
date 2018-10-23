d9e21d07a5b0b3014e5aec08aa8179e4cc8aef4b
florian <prigent.florian.emile@gmail.com>
2018-10-23 11:49:24 +0200

    update flop

--- cdr_robot_ws/src/software/src/robot_path/Map.py
+++ cdr_robot_ws/src/software/src/robot_path/Map.py
@@ -3,8 +3,8 @@
 import numpy as np
 import numpy.linalg as alg
 
-import Obstacle from Obstacle
-import Robot from Robot
+from Obstacle import Obstacle
+from Robot import Robot
 
 class Map:
     def __init__(self, width, length, width_resolution, length_resolution):
@@ -16,14 +16,18 @@ class Map:
         self.obstacles_= set()
         self.robots_ = set()
 
-    def Add_Robot(self, robot):
-        self.matrix_[robot.pose2D] = 2 #Robot is on the map and set to 2
-
-    def Add_Obstacle(self, obstacle):
-        self.matrix_[obstacle.pose2D] = 1 #Obstacle i son the map and set to 1
-
     def Pose_Available(self, piece):
         if(self.matrix_[piece.pose2D] == 0):
             return True
         else:
             return False
+
+    def Add_Robot(self, robot):
+        if(Pose_Available(self,robot) == True):
+            self.matrix_[robot.pose2D.x , robot.pose2D.y] = 2 #Robot is on the map and set to 2
+
+    def Add_Obstacle(self, obstacle):
+        if(Pose_Available(self,obstacle) == True):
+        self.matrix_[obstacle.pose2D] = 1 #Obstacle i son the map and set to 1
+
+    
