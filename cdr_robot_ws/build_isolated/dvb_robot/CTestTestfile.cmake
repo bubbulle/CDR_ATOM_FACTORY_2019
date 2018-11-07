f3bfe54f2f038eab7e8eb184fe22069cd21ea578
Florian Quibel <florian.quibel27@gmail.com>
2018-11-07 15:23:36 +0100

    update

--- cdr_robot_ws/build_isolated/dvb_robot/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/florian/Documents/dvb_ws/CDR_ATOM_FACTORY_2019/cdr_robot_ws/src/dvb_robot
-# Build directory: /home/florian/Documents/dvb_ws/CDR_ATOM_FACTORY_2019/cdr_robot_ws/build_isolated/dvb_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
