2111c9abac7595d1e1c6c69947f8892acad39a63
Florian Quibel <florian.quibel27@gmail.com>
2018-11-12 10:56:15 +0100

    add ros dvb lib

---
+++ cdr_robot_ws/build/CTestTestfile.cmake
@@ -0,0 +1,15 @@
+# CMake generated Testfile for 
+# Source directory: /home/florian/Documents/dvb_ws/CDR_ATOM_FACTORY_2019/cdr_robot_ws/src
+# Build directory: /home/florian/Documents/dvb_ws/CDR_ATOM_FACTORY_2019/cdr_robot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(param_controller)
+subdirs(pid_controller)
+subdirs(ROS_Libraries/dvb_hardware)
+subdirs(ROS_Libraries/dvb_robot)
+subdirs(ROS_Libraries/dvb_map)
+subdirs(encoder_controller)
+subdirs(motor_controller)
+subdirs(robot_path_planning)
